e87fe472756decd3cd6b7e74e01fc8d3dfe11e8e
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-04-08 15:32:07 +0200

    Remove everything except Auto mode

--- Sample.UI/MainWindow.xaml.cs
+++ Sample.UI/MainWindow.xaml.cs
@@ -26,155 +26,58 @@ namespace Sample.UI
     public partial class MainWindow : Window
     {
         private static readonly MediaManager mediaManager = new MediaManager();
-        private static MediaSession? currentSession = null;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            mediaManager.OnAnySessionOpened += MediaManager_OnAnySessionOpened;
-            mediaManager.OnAnySessionClosed += MediaManager_OnAnySessionClosed;
+            mediaManager.OnAnyPlaybackStateChanged += MediaManager_OnAnyPlaybackStateChanged;
 
             mediaManager.Start();
-        }
 
-        private void MediaManager_OnAnySessionOpened(MediaSession mediaSession)
-        {
-            Application.Current.Dispatcher.Invoke(() =>
+            var menuItem = new NavigationViewItem
             {
-                var menuItem = new NavigationViewItem
+                Content = "Automatic",
+                Icon = new SymbolIcon() { Symbol = Symbol.Globe },
+                Tag = null
+            };
+            SongList.MenuItems.Insert(0, menuItem);
+            SongList.SelectedItem = menuItem;
+
+            Application.Current.Dispatcher.Invoke(async () =>
+            {
+                foreach (var mediaSession in mediaManager.CurrentMediaSessions.Values)
                 {
-                    Content = mediaSession.Id,
-                    Icon = new SymbolIcon() { Symbol = Symbol.Audio },
-                    Tag = mediaSession
-                };
-                SongList.MenuItems.Add(menuItem);
+                    var playbackInfo = mediaSession.ControlSession.GetPlaybackInfo();
+                    if (playbackInfo.PlaybackStatus == GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing)
+                    {
+                        await UpdateUI(mediaSession);
+                        break;
+                    }
+                }
             });
         }
 
-        private void MediaManager_OnAnySessionClosed(MediaSession session)
+        private void MediaManager_OnAnyPlaybackStateChanged(MediaSession mediaSession, GlobalSystemMediaTransportControlsSessionPlaybackInfo playbackInfo)
         {
-            Application.Current.Dispatcher.Invoke(() =>
+            if (playbackInfo.PlaybackStatus != GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing)
             {
-                NavigationViewItem? itemToRemove = null;
+                return;
+            }
 
-                foreach (NavigationViewItem? item in SongList.MenuItems)
-                        if (item?.Content.ToString() == session.Id)
-                            itemToRemove = item;
-
-                if(itemToRemove != null)
-                    SongList.MenuItems.Remove(itemToRemove);
+            Application.Current.Dispatcher.Invoke(async () => { 
+                await UpdateUI(mediaSession);
             });
         }
 
-        private void SongList_SelectionChanged(NavigationView navView, NavigationViewSelectionChangedEventArgs args)
+        private async Task UpdateUI(MediaSession mediaSession)
         {
-            if(currentSession != null)
-            {
-                currentSession.OnMediaPropertyChanged -= CurrentSession_OnMediaPropertyChanged;
-                currentSession.OnPlaybackStateChanged -= CurrentSession_OnPlaybackStateChanged;
-                currentSession = null;
-            }
-
-            if(navView.SelectedItem != null)
-            {
-                currentSession = (MediaSession)((NavigationViewItem)navView.SelectedItem).Tag;
-                currentSession.OnMediaPropertyChanged += CurrentSession_OnMediaPropertyChanged;
-                currentSession.OnPlaybackStateChanged += CurrentSession_OnPlaybackStateChanged;
-                CurrentSession_OnPlaybackStateChanged(currentSession);
-            }
-            else
-            {
-                SongImage.Source = null;
-                SongTitle.Content = "TITLE";
-                SongAuthor.Content = "Author";
-                ControlPlayPause.Content = "▶️";
-            }
-        }
-
-        private void CurrentSession_OnPlaybackStateChanged(MediaSession mediaSession, GlobalSystemMediaTransportControlsSessionPlaybackInfo? playbackInfo = null)
-        {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                UpdateUI(mediaSession);
-            });
-        }
-
-        private void CurrentSession_OnMediaPropertyChanged(MediaSession mediaSession, GlobalSystemMediaTransportControlsSessionMediaProperties mediaProperties)
-        {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                UpdateUI(mediaSession);
-            });
-        }
-
-        private void UpdateUI(MediaSession mediaSession)
-        {
-            var mediaProp = mediaSession.ControlSession.GetPlaybackInfo();
-            if (mediaProp != null)
-            {
-                if (mediaSession.ControlSession.GetPlaybackInfo().Controls.IsPauseEnabled)
-                    ControlPlayPause.Content = "II";
-                else
-                    ControlPlayPause.Content = "▶️";
-                ControlBack.IsEnabled = ControlForward.IsEnabled = mediaProp.Controls.IsNextEnabled;
-            }
-
-            var songInfo = mediaSession.ControlSession.TryGetMediaPropertiesAsync().GetAwaiter().GetResult();
+            var songInfo = await mediaSession.ControlSession.TryGetMediaPropertiesAsync();
             if(songInfo != null)
             {
                 SongTitle.Content = songInfo.Title.ToUpper();
-                SongAuthor.Content = songInfo.Artist;
-                SongImage.Source = Helper.GetThumbnail(songInfo.Thumbnail);
+                SongAuthor.Content = songInfo.Artist.Replace(" - Topic", "", StringComparison.InvariantCultureIgnoreCase);
             }
-
-        }
-
-        private async void Back_Click(object sender, RoutedEventArgs e)
-        {
-            await currentSession?.ControlSession.TrySkipPreviousAsync();
-        }
-
-        private async void PlayPause_Click(object sender, RoutedEventArgs e)
-        {
-            var controlsInfo = currentSession?.ControlSession.GetPlaybackInfo().Controls;
-
-            if (controlsInfo?.IsPauseEnabled == true)
-                await currentSession?.ControlSession.TryPauseAsync();
-            else if (controlsInfo?.IsPlayEnabled == true)
-                await currentSession?.ControlSession.TryPlayAsync();
-        }
-
-        private async void Forward_Click(object sender, RoutedEventArgs e)
-        {
-            await currentSession?.ControlSession.TrySkipNextAsync();
-        }
-    }
-
-    internal static class Helper
-    {
-        internal static BitmapImage? GetThumbnail(IRandomAccessStreamReference Thumbnail)
-        {
-            if (Thumbnail == null)
-                return null;
-
-            var imageStream = Thumbnail.OpenReadAsync().GetAwaiter().GetResult();
-            byte[] fileBytes = new byte[imageStream.Size];
-            using (DataReader reader = new DataReader(imageStream))
-            {
-                reader.LoadAsync((uint)imageStream.Size).GetAwaiter().GetResult();
-                reader.ReadBytes(fileBytes);
-            }
-
-            var image = new BitmapImage();
-            using (var ms = new System.IO.MemoryStream(fileBytes))
-            {
-                image.BeginInit();
-                image.CacheOption = BitmapCacheOption.OnLoad;
-                image.StreamSource = ms;
-                image.EndInit();
-            }
-            return image;
         }
     }
 }
