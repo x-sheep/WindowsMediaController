3e83a5adf8f0f9e24cea067a43f8a8c7c537d8e4
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-04-08 15:58:56 +0200

    Remove input source menu

--- Sample.UI/MainWindow.xaml.cs
+++ Sample.UI/MainWindow.xaml.cs
@@ -26,55 +26,89 @@ namespace Sample.UI
     public partial class MainWindow : Window
     {
         private static readonly MediaManager mediaManager = new MediaManager();
+        private string? lastMediaSession = null;
 
         public MainWindow()
         {
             InitializeComponent();
 
             mediaManager.OnAnyPlaybackStateChanged += MediaManager_OnAnyPlaybackStateChanged;
+            mediaManager.OnAnySessionOpened += MediaManager_OnAnySessionOpened;
+            mediaManager.OnAnySessionClosed += MediaManager_OnAnySessionClosed;
 
             mediaManager.Start();
 
-            var menuItem = new NavigationViewItem
-            {
-                Content = "Automatic",
-                Icon = new SymbolIcon() { Symbol = Symbol.Globe },
-                Tag = null
-            };
-            SongList.MenuItems.Insert(0, menuItem);
-            SongList.SelectedItem = menuItem;
-
             Application.Current.Dispatcher.Invoke(async () =>
             {
-                foreach (var mediaSession in mediaManager.CurrentMediaSessions.Values)
-                {
-                    var playbackInfo = mediaSession.ControlSession.GetPlaybackInfo();
-                    if (playbackInfo.PlaybackStatus == GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing)
-                    {
-                        await UpdateUI(mediaSession);
-                        break;
-                    }
-                }
+                await LoadRandomSession();
             });
         }
 
-        private void MediaManager_OnAnyPlaybackStateChanged(MediaSession mediaSession, GlobalSystemMediaTransportControlsSessionPlaybackInfo playbackInfo)
+        private void MediaManager_OnAnySessionOpened(MediaSession mediaSession)
         {
+            var playbackInfo = mediaSession.ControlSession.GetPlaybackInfo();
             if (playbackInfo.PlaybackStatus != GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing)
             {
                 return;
             }
 
-            Application.Current.Dispatcher.Invoke(async () => { 
+            Application.Current.Dispatcher.Invoke(async () =>
+            {
                 await UpdateUI(mediaSession);
+            });
+        }
+
+        private void MediaManager_OnAnySessionClosed(MediaSession mediaSession)
+        {
+            if (lastMediaSession == mediaSession.Id)
+            {
+                Application.Current.Dispatcher.Invoke(async () =>
+                {
+                    await LoadRandomSession();
+                });
+            }
+        }
+
+        private async Task LoadRandomSession()
+        {
+            foreach (var mediaSession in mediaManager.CurrentMediaSessions.Values)
+            {
+                var playbackInfo = mediaSession.ControlSession.GetPlaybackInfo();
+                if (playbackInfo.PlaybackStatus == GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing)
+                {
+                    await UpdateUI(mediaSession);
+                    return;
+                }
+            }
+
+            StatusText.Content = "No session opened";
+            SongTitle.Content = "";
+            SongAuthor.Content = "";
+        }
+
+        private void MediaManager_OnAnyPlaybackStateChanged(MediaSession mediaSession, GlobalSystemMediaTransportControlsSessionPlaybackInfo playbackInfo)
+        {
+            var isPlaying = playbackInfo.PlaybackStatus == GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing;
+
+            Application.Current.Dispatcher.Invoke(async () =>
+            {
+                if (isPlaying)
+                    await UpdateUI(mediaSession);
+                else if (lastMediaSession == mediaSession.Id)
+                {
+                    lastMediaSession = null;
+                    await LoadRandomSession();
+                }
             });
         }
 
         private async Task UpdateUI(MediaSession mediaSession)
         {
+            lastMediaSession = mediaSession.Id;
             var songInfo = await mediaSession.ControlSession.TryGetMediaPropertiesAsync();
-            if(songInfo != null)
+            if (songInfo != null)
             {
+                StatusText.Content = "Playing from " + lastMediaSession;
                 SongTitle.Content = songInfo.Title.ToUpper();
                 SongAuthor.Content = songInfo.Artist.Replace(" - Topic", "", StringComparison.InvariantCultureIgnoreCase);
             }
